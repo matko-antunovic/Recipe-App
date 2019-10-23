bbd2a1846ee7374be922ca968b7e99ae7d3a9abf
matko-antunovic <47244433+matko-antunovic@users.noreply.github.com>
2019-10-23 18:13:46 +0200

    first

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from "./models/Search"
 import * as searchView from "./views/searchView"
-import {elements} from "./views/base"
+import {elements,renderLoader,clearLoader} from "./views/base"
 
 //GLOBAL APP STATE
 
@@ -19,10 +19,13 @@ const controlSearch=async ()=>{
         //spinner,reset UI 
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
+
         //search for recipes
         await state.search.getData();
 
         //render UI
+        clearLoader();
         searchView.renderResults(state.search.result)
     }
 
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -5,7 +5,6 @@ export default class Search{
         this.query=query;
     }
     async getData(){
-    const proxy="https://cors-anywhere.herokuapp.com/";
     const key="4ff6aaa721ebf30f34b8ce8a16051103";
     try{
         const res=await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
